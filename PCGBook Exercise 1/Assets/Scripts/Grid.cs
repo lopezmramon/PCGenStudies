4b5e1320e8d64fda55c99619af3b16021843017a
Ramon Lopez <ramon@fastmicroservices.com>
2018-06-28 11:17:17 +0700

    Starting with Exercise 1

---
+++ PCGBook Exercise 1/Assets/Scripts/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public int sizeX;
+    public int sizeY;
+    public GameObject[] prefabs;
+    public GameObject[,] gameObjects;
+    GridData gridData;
+    WaitForSeconds generationThrottle;
+
+    private void Awake()
+    {
+        generationThrottle = new WaitForSeconds(0.1f);
+    }
+
+    void Start()
+    {
+    }
+
+    IEnumerator InstantiateGrid(GridData gridData)
+    {
+        gameObjects = new GameObject[gridData.map.GetLength(0), gridData.map.GetLength(1)];
+
+        for (int i = 0; i < gridData.map.GetLength(0); i++)
+        {
+            for (int j = 0; j < gridData.map.GetLength(1); j++)
+            {
+                yield return generationThrottle;
+                gameObjects[i, j] = Instantiate(prefabs[gridData.map[i, j]], positionFromCoordinates(i, j), Quaternion.identity);
+            }
+        }
+    }
+
+    Vector3 positionFromCoordinates(int x, int y)
+    {
+        return new Vector3(x, y, 0);
+    }
+}
---
+++ PCGBook Exercise 1/Assets/Scripts/GridData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridData {
+
+    public int[,] map;
+    public GridData GenerateGrid(int x, int y)
+    {
+        map = new int[x, y];
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                map[i, j] = 0;
+            }
+        }
+
+        return null;
+    }
+
+}
